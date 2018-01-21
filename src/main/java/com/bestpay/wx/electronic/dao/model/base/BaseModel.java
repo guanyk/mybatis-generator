b43f16829a0b389386313275286e9d6c74b3b8e3
HUANGXIYAO <HUANGXIYAO@BESTPAY.COM.CN>
2018-01-21 20:38:57 +0800

    修改配置文件，增加继承basemodel；自定义列类型转换

---
+++ src/main/java/com/bestpay/wx/electronic/dao/model/base/BaseModel.java
@@ -0,0 +1,38 @@
+package com.hxy.base;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by HUANGXIYAO on 2017/11/23.
+ */
+@Setter
+@Getter
+@ToString
+public class BaseModel implements Serializable{
+    private static final long serialVersionUID = -3665540730986053048L;
+    /**
+     * 主键id，自增长
+     */
+    private long id;
+    /**
+     * 创建人
+     */
+    private String createdBy;
+    /**
+     * 创建时间
+     */
+    private Date createdAt;
+    /**
+     * 更新人
+     */
+    private String updatedBy;
+    /**
+     * 更新时间
+     */
+    private Date updatedAt;
+}
